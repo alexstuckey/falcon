94207b8eed6e7af59819af6dc1f52eab04584ebe
Alex Stuckey <alex@stuckey.org.uk>
2013-08-29 11:05:07 +0100

    Basic method selection and usage
    Constructs the path for the method.js file by using the method name passed in the RabbitMQ request. This is a security/stability issue and should be fixed. Perhaps with an array of approved method files.

--- onMessage.js
+++ onMessage.js
@@ -1,5 +1,12 @@
 module.exports = function(message, headers, deliveryInfo, queue){
-  payload = JSON.parse(message.data.toString('utf-8'))
+  var payload = JSON.parse(message.data.toString('utf-8'))
+  console.log("Received message: ", payload)
+
+  var method_string = './methods/' + payload[1] + '.js'
+  var method = require(method_string)
+  console.log("Using method:", method.info['name'])
+
+  method.send(payload[3], payload[2])
 
   // Acknowledges the message, should only be done if the notification
   // has been successfully processed. Need to check if it has been first.
