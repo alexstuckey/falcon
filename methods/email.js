2ee61e617142ad0848893fb4802ac81416615207
Alex Stuckey <alex@stuckey.org.uk>
2013-08-29 11:08:54 +0100

    Basic email sending and account setup
    Using the default settings framework for Gmail. Loads the username and password from the configuration file. Needs to be tidied.

--- methods/email.js
+++ methods/email.js
@@ -3,5 +3,36 @@ exports.info = {
 }
 
 exports.send = function(address, plainbody) {
+  var fs = require('fs')
+  // Load the configuration JSON file
+  var config = JSON.parse(fs.readFileSync('./config.json'))
 
+  var nodemailer = require('nodemailer')
+
+  var transport = nodemailer.createTransport("SMTP", {
+    service: "gmail",
+    auth: {
+      user: config.SMTP.username,
+      pass: config.SMTP.password
+    }
+
+  })
+
+  var email = {
+    from: config.SMTP.from,
+    to: address,
+    subject: "Here it goes again",
+    text: plainbody
+  }
+
+  console.log("   sending mail", email)
+
+  transport.sendMail(email, function(error){
+    if(error){
+      console.log("error occured")
+      console.log(error.message)
+      return;
+    }
+    console.log("Send successful")
+  })
 }
