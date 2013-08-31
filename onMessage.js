fba222c4674b6bdf86a57564e3d904f2b1a38e11
Alex Stuckey <alex@stuckey.org.uk>
2013-09-01 00:19:14 +0100

    Checks against a whitelist of supported methods

--- onMessage.js
+++ onMessage.js
@@ -1,14 +1,26 @@
 module.exports = function(message, headers, deliveryInfo, queue){
+  var fs = require('fs')
+  // Load the configuration JSON file
+  var config = JSON.parse(fs.readFileSync('./config.json'))
+
   var payload = JSON.parse(message.data.toString('utf-8'))
+
   console.log("Received message: ", payload)
 
-  var method_string = './methods/' + payload[1] + '.js'
-  var method = require(method_string)
-  console.log("Using method:", method.info['name'])
+  if (config.supportedMethods.indexOf(payload[1])==-1) {
+    console.log('Unsupproted method:', payload[1])
+  } else {
+    
+    var method_string = './methods/' + payload[1] + '.js'
+    var method = require(method_string)
+    console.log("Using method:", method.info['name'])
 
-  method.send(payload[3], payload[2])
+    method.send(payload[3], payload[2])
+
+  }
 
   // Acknowledges the message, should only be done if the notification
   // has been successfully processed. Need to check if it has been first.
   queue.queue.shift()
+
 }
