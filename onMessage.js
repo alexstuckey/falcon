2afa56466beddb2698dc59411e04d3320775ee77
Alex Stuckey <alex@stuckey.org.uk>
2013-09-01 15:41:58 +0100

    Assigning complex ifs to variables

--- onMessage.js
+++ onMessage.js
@@ -10,20 +10,21 @@ module.exports = function(message, headers, deliveryInfo, queue){
 
   console.log("Received message: ", payload);
 
-  if (config.supportedMethods.indexOf(payload[1])==-1) {
+  var isSupported = (config.supportedMethods.indexOf(payload[1])==-1);
+
+  if (isSupported) {
     console.log('Unsupproted method:', payload[1]);
   } else {
-    
     var method_string = './methods/' + payload[1] + '.js';
     var method = require(method_string);
     console.log("Using method:", method.info['name']);
 
+    // If simulate flag has been set
     if (!argv.s) {
       method.send(payload[3], payload[2]);
     } else {
       console.log("Simulating message send");
     }
-
   }
 
   // Acknowledges the message, should only be done if the notification
